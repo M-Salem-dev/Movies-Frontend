9fea095051a9b5a12a848d0dc3e7822c4d5c3be9
M-Salem-dev <mohamed.salem.dev@gmail.com>
2023-07-22 22:13:11 +0300

    first commit

---
+++ ده علشان النفربار الجانبي الحاجات اللي جوه تتحرك بتتدريج.js
@@ -0,0 +1,7 @@
+
+$(".nav-tab-menu .item1").animate({opacity:"1",paddingTop:"25px"},1100),
+$(".nav-tab-menu .item2").animate({opacity:"1",paddingTop:"25px"},1200),
+$(".nav-tab-menu .item3").animate({opacity:"1",paddingTop:"25px"},1300),
+$(".nav-tab-menu .item4").animate({opacity:"1",paddingTop:"25px"},1400),
+$(".nav-tab-menu .item5").animate({opacity:"1",paddingTop:"25px"},1500),
+$(".nav-tab-menu .item6").animate({opacity:"1",paddingTop:"25px"},1600),
